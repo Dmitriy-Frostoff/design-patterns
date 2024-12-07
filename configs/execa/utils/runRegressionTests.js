8dadfcec8ef1f70d0f1e4ec14bb1c52350b7d40a
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2024-12-07 21:55:04 +0300

    refactor(execa): break main.js script's logic into modules, add clear messages writing to the update-error.log

---
+++ configs/execa/utils/runRegressionTests.js
@@ -0,0 +1,27 @@
+import { $, ExecaError } from 'execa';
+
+/**
+ * Run regression tests.
+ *
+ * @param {(pathToLogFile: string, error: Error | ExecaError) => Promise<void>} writeErrorLogFile - callback for
+ *    async writing log file via NodeJS
+ * @param {string} logFile - resolved via NodeJS path to the logfile
+ * @returns {Promise<void>}
+ */
+export default async function runRegressionTests(writeErrorLogFile, logFile) {
+  try {
+    console.log('Run regression tests...\n');
+    await $(
+      `npm run tsx creational/abstract-factory/abstract-factory.ts && npm run tsx behavioral/observer/observer.ts && npm run tsx structural/decorator/decorator.ts`,
+      {
+        stdio: ['pipe', 'pipe', 'pipe'],
+        cleanup: true,
+      },
+    );
+  } catch (error) {
+    console.error(`An error occured: ${error.message}`);
+
+    // write logfile beside the script
+    await writeErrorLogFile(logFile, error);
+  }
+}
