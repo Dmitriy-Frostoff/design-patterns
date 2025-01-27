53d76633428e39abd0df0854e695c884bfbcb0ca
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-01-27 23:10:54 +0300

    refactor(factory-method pattern): modify BmwFactory class to abstract

--- creational/factory-method/BmwFactory.ts
+++ creational/factory-method/BmwFactory.ts
@@ -1,6 +1,6 @@
 import Bmw from './Bmw';
 
-export default class BmwFactory {
+export default abstract class BmwFactory {
   public static create(type: string): Bmw {
     if (type === 'X5') return new Bmw(type, 108000, 300);
     if (type === 'X6') return new Bmw(type, 111000, 320);
